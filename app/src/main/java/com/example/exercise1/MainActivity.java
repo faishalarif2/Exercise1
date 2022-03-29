34e0284cc3e7269a1e6fa3a97e9cf8fcfa5166cc
faishalarif2 <faishalarifn02@gmail.com>
2022-03-29 21:46:14 +0700

    Tambahan

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
 
         //Menghubungkan variabel btnLogin dengan componen button pada layout
         btnLogin = findViewById(R.id.btSignIn);
